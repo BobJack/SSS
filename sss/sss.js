74541f4bf3598c29f2607ed86dbdc4bb2efd395b
Serge Sysoev <admin@9-may.info>
2016-05-12 15:15:33 +0300

    check code style, display images on ready

--- sss/sss.js
+++ sss/sss.js
@@ -7,11 +7,11 @@ $.fn.sss = function(options) {
 // Options
 
 	var settings = $.extend({
-	slideShow : true,
-	startOn : 0,
-	speed : 3500,
-	transition : 400,
-	arrows : true
+		slideShow : true,
+		startOn : 0,
+		speed : 3500,
+		transition : 400,
+		arrows : true
 	}, options);
 
 	return this.each(function() {
@@ -36,67 +36,68 @@ $.fn.sss = function(options) {
 // Reset Slideshow
 
 	reset_timer = settings.slideShow ? function() {
-	clearTimeout(timer);
-	timer = setTimeout(next_slide, settings.speed);
+		clearTimeout(timer);
+		timer = setTimeout(next_slide, settings.speed);
 	} : $.noop;
 
 // Animate Slider
 
 	function get_height(target) {
-	return ((slides.eq(target).height() / slider.width()) * 100) + '%';
+		return ((slides.eq(target).height() / slider.width()) * 100) + '%';
 	}
 
 	function animate_slide(target) {
-	if (!animating) {
-	animating = true;
-	var target_slide = slides.eq(target);
+		if (!animating) {
+			animating = true;
+			var target_slide = slides.eq(target);
 
-	target_slide.fadeIn(transition);
-	slides.not(target_slide).fadeOut(transition);
+			target_slide.fadeIn(transition);
+			slides.not(target_slide).fadeOut(transition);
 
-	slider.animate({paddingBottom: get_height(target)}, transition,  function() {
-	animating = false;
-	});
+			slider.animate({paddingBottom: get_height(target)}, transition,  function() {
+				animating = false;
+			});
 
-	reset_timer();
+			reset_timer();
 
-	}};
+		}
+	};
 
 // Next Slide
 
 	function next_slide() {
-	target = target === slide_count - 1 ? 0 : target + 1;
-	animate_slide(target);
+		target = target === slide_count - 1 ? 0 : target + 1;
+		animate_slide(target);
 	}
 
 // Prev Slide
 
 	function prev_slide() {
-	target = target === 0 ? slide_count - 1 : target - 1;
-	animate_slide(target);
+		target = target === 0 ? slide_count - 1 : target - 1;
+		animate_slide(target);
 	}
 
 	if (settings.arrows) {
-	slider.append('<div class="sssprev"/>', '<div class="sssnext"/>');
+		slider.append('<div class="sssprev"/>', '<div class="sssnext"/>');
 	}
 
 	next = slider.find('.sssnext'),
 	prev = slider.find('.sssprev');
 
-	$(window).load(function() {
+	$(window).ready(function() {
 
 	slider.css({paddingBottom: get_height(target)}).click(function(e) {
-	clicked = $(e.target);
-	if (clicked.is(next)) { next_slide() }
-	else if (clicked.is(prev)) { prev_slide() }
+		clicked = $(e.target);
+		if (clicked.is(next)) { next_slide() }
+		else if (clicked.is(prev)) { prev_slide() }
 	});
 
 	animate_slide(target);
 
 	$(document).keydown(function(e) {
-	key = e.keyCode;
-	if (key === 39) { next_slide() }
-	else if (key === 37) { prev_slide() }
+		key = e.keyCode;
+		if (key === 39) { next_slide() }
+		else if (key === 37) { prev_slide() }
 	});
 
 	});
